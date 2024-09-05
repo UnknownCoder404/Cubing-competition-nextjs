3ff3fa11b17bd34c43598eca52458183a7a6b32f
Jakov <qwerasdftzu@gmail.com>
2024-09-05 12:59:09 +0200

    Little bit of refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../../Styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1>Cubing Competition</h1>
+        {children}
+      </body>
     </html>
   );
 }
