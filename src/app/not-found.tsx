d70bff501bc5bc5d61614ac7007980d5b95d1ef8
Jakov <qwerasdftzu@gmail.com>
2024-09-08 13:17:04 +0200

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Stranica nije pronađena</h1>
+      <p>Stranica koju ste pokušali posjetiti nije pronađena. Nemojte se brinuti, možda je to greška u URL-u.</p>
+      <ul>
+        <li>
+          <Link href="/">Povratak na početnu stranicu</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
