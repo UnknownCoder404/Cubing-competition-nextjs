00295bad81467346010887d5b669bfd80444a9a4
Jakov <qwerasdftzu@gmail.com>
2024-09-14 20:53:09 +0200

    Styling improvements for larger screens

--- src/app/Scramble/page.tsx
+++ src/app/Scramble/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import ScrambleDisplayComponent from "../components/Scramble/ScrambleDisplay";
+import ScrambleDisplay from "../components/Scramble/ScrambleDisplay";
 import getScramble from "./getScramble";
 import scrambleStyles from "./Scramble.module.css";
 
@@ -13,20 +13,24 @@ export default function ScramblePage() {
   return (
     <main className={scrambleStyles["main"]}>
       <p className={scrambleStyles["scramble"]}>{scramble}</p>
-      <ScrambleDisplayComponent
-        scramble={scramble}
-        event="333"
-        visualization="2D"
-        containerClassName={scrambleStyles["scramble-display-container"]}
-      />
-      <button
-        className={scrambleStyles["btn-rescramble"]}
-        onClick={() => {
-          generateNewScramble(setScramble);
-        }}
-      >
-        Promiješaj
-      </button>
+      <div className={scrambleStyles["scramble-controls"]}>
+        <ScrambleDisplay
+          scramble={scramble}
+          event="333"
+          visualization="2D"
+          containerClassName={scrambleStyles["scramble-display-container"]}
+        />
+        <div className={scrambleStyles["btn-rescramble-container"]}>
+          <button
+            className={scrambleStyles["btn-rescramble"]}
+            onClick={() => {
+              generateNewScramble(setScramble);
+            }}
+          >
+            Promiješaj
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
