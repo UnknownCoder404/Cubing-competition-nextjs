cfeeccee43d5c6f55330e9c88a1bcda1cfe26df8
Jakov <qwerasdftzu@gmail.com>
2024-09-14 15:28:55 +0200

    Show username when loaded

--- src/app/components/Header/ClientLoginStatus.tsx
+++ src/app/components/Header/ClientLoginStatus.tsx
@@ -1,33 +1,38 @@
 "use client"; // This makes this component a client-side component
 import Link from "next/link";
 import headerStyles from "./Header.module.css";
-import { getUsername, loggedIn, logOut } from "@/app/utils/credentials";
+import { getUsername, logOut } from "@/app/utils/credentials";
 import accountCircle from "../../public/account_circle.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 function ClientLoginStatus() {
-  const username = getUsername();
-  const isloggedIn = loggedIn();
+  const [username, setUsername] = useState<string | null>(null);
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const loggedIn = !!username;
+  useEffect(() => {
+    setUsername(getUsername());
+    setLoaded(true);
+  }, []);
+  if (!loaded) {
+    return <div className={headerStyles["account-container"]}></div>;
+  }
   return (
     <>
-      {isloggedIn ? (
-        <h2 className={headerStyles["log-in"]}>{username}</h2>
-      ) : (
-        <h2 className={headerStyles["log-in"]}>
-          <Link href="Login/">Prijava</Link>
-        </h2>
-      )}
+      <h2 className={headerStyles["log-in"]}>
+        {username ? username : <Link href="Login/">Prijava</Link>}
+      </h2>
 
       <Image
         alt="account circle"
         className={`${headerStyles["account-circle"]} ${
-          isloggedIn ? headerStyles["logged-in"] : ""
+          loggedIn ? headerStyles["logged-in"] : ""
         }`}
         src={accountCircle}
         width={24}
         height={24}
         onClick={() => {
-          if (isloggedIn) {
+          if (loggedIn) {
             logOut();
             window.location.reload(); // Refresh the page after logout
           }
