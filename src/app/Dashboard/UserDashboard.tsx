62bff5b424cda50cdf3cfb1fbe0c02b14c26a82d
Jakov <qwerasdftzu@gmail.com>
2024-09-20 18:23:32 +0200

    Add all server side for dashboard

--- src/app/Dashboard/UserDashboard.tsx
+++ src/app/Dashboard/UserDashboard.tsx
@@ -1,12 +1,37 @@
 import { User } from "./page";
 import dashboardStyles from "./Dashboard.module.css";
+import manageAccounts from "../public/manage_accounts.svg";
+import Image from "next/image";
+
+function UsernameDiv({ username }: { username: string }) {
+  return (
+    <div className={dashboardStyles["username-div"]}>
+      <p className={dashboardStyles["username"]}>{username}</p>
+      <Image
+        className={dashboardStyles["manage-accounts"]}
+        src={manageAccounts}
+        width={undefined}
+        height={24}
+        alt="Manage accounts"
+      />
+    </div>
+  );
+}
+
+function UserInfo({ user }: { user: User }) {
+  return (
+    <>
+      <UsernameDiv username={user.username} />
+      <p className={dashboardStyles["role"]}>Role: {user.role}</p>
+      <p className={dashboardStyles["group"]}>Grupa: {user.group}</p>
+    </>
+  );
+}
 
 export default function UserDashboard({ user }: { user: User }) {
   return (
     <div className={dashboardStyles["user"]}>
-      <h1>{user.username}</h1>
-      <p>Group: {user.group}</p>
-      <p>Role: {user.role}</p>
+      <UserInfo user={user} />
     </div>
   );
 }
