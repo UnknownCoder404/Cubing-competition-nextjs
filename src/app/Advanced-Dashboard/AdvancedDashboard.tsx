0bd20f2e29d245713e88fe0ec2efa40a12602e29
Jakov <qwerasdftzu@gmail.com>
2024-09-26 12:48:23 +0200

    Add mock for advanced dashboard

---
+++ src/app/Advanced-Dashboard/AdvancedDashboard.tsx
@@ -0,0 +1,11 @@
+import { CompetitionResultType, Users } from "../Types/solve";
+
+export default function AdvancedDashboard({
+  users,
+  competitions,
+}: {
+  users: Users;
+  competitions: CompetitionResultType[];
+}) {
+  return <div></div>;
+}
