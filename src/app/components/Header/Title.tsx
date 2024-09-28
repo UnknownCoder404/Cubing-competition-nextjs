a9f22bf14933a7a342451f5891ac246d04aba65a
Jakov <qwerasdftzu@gmail.com>
2024-09-28 22:08:10 +0200

    Fix all build warnings

--- src/app/components/Header/Title.tsx
+++ src/app/components/Header/Title.tsx
@@ -1,23 +1,24 @@
-"use client"; // Mark this as a client-side component
+"use client";
+
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import headerStyles from "./Header.module.css";
 
+const routeTitles: {
+  [index: string]: string | undefined;
+} = {
+  "/": "Cro Cube Comp",
+  "/Login": "Prijava",
+  "/Register": "Registracija",
+  "/Competitions": "Natjecanja",
+  "/Scramble": "Vježbanje",
+  "/Dashboard": "Radna ploča",
+  "/Advanced-Dashboard": "Radna ploča",
+};
+
 function ClientTitle() {
   const pathname = usePathname(); // Get the current pathname
   const [title, setTitle] = useState<string>("");
-
-  const routeTitles: {
-    [index: string]: string | undefined;
-  } = {
-    "/": "Cro Cube Comp",
-    "/Login": "Prijava",
-    "/Register": "Registracija",
-    "/Competitions": "Natjecanja",
-    "/Scramble": "Vježbanje",
-    "/Dashboard": "Radna ploča",
-    "/Advanced-Dashboard": "Radna ploča",
-  };
 
   useEffect(() => {
     // Update the title based on the current pathname
