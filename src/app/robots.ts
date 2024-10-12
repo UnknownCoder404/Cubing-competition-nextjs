f1e82c97503e8c8adaf9fe249f17dbcfd3c9f672
Jakov <qwerasdftzu@gmail.com>
2024-10-12 18:08:37 +0200

    Add robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+const robots = (): MetadataRoute.Robots => ({
+  rules: [
+    {
+      userAgent: "*",
+      allow: "*",
+      disallow: ["/Login", "/Register", "/Advanced-Dashboard", "/Dashboard"],
+    },
+  ],
+});
+
+export default robots;
+
+// Enforces that this route is used as static rendering
+// @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = "error";
