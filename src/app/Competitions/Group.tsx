d6632bd59a8fa46c2571825a2d4078ad98a6e6a9
Jakov <qwerasdftzu@gmail.com>
2024-10-18 17:59:43 +0200

    Seperate Group and Group results & Add loader for group results (because of framer-motion library)

--- src/app/Competitions/Group.tsx
+++ src/app/Competitions/Group.tsx
@@ -1,18 +1,29 @@
 "use client";
 import { useState } from "react";
 import { Result } from "../Types/solve";
-import { motion } from "framer-motion";
 import CompetitionStyles from "./Competitions.module.css";
-import Round from "./Round";
 import ShowAndHide from "../components/Competitions/showAndHide";
+import dynamic from "next/dynamic";
+import { Loader } from "../components/Loader/Loader";
+const GroupResults = dynamic(() => import("./GroupResults"), {
+  ssr: false,
+  loading: () => <LoadingGroup />,
+});
 
-export default function Group({
-  group,
-  groupNumber,
-}: {
+function LoadingGroup() {
+  return (
+    <div className={CompetitionStyles["group-loader"]}>
+      <Loader />
+    </div>
+  );
+}
+
+type Props = {
   group: Result[][];
   groupNumber: number;
-}) {
+};
+
+export default function Group({ group, groupNumber }: Props) {
   const [areGroupResultsShown, setGroupResultsVisibility] = useState(true);
 
   // Initialize an array of visibility states, one for each round
@@ -51,31 +62,12 @@ export default function Group({
         />
       </div>
 
-      {/* Animate height and add padding/margin collapse */}
-      <motion.div
-        className={`${CompetitionStyles["group-results"]}`}
-        initial={{ height: 0, opacity: 0, paddingTop: 0, paddingBottom: 0 }}
-        animate={{
-          height: areGroupResultsShown ? "auto" : 0,
-          opacity: areGroupResultsShown ? 1 : 0,
-          paddingTop: areGroupResultsShown ? "1rem" : 0, // Adjust based on your styles
-          paddingBottom: areGroupResultsShown ? "1rem" : 0, // Adjust based on your styles
-        }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-        style={{ overflow: "hidden" }} // Ensure the content doesn't overflow when hidden
-      >
-        {group.map((round, index) => {
-          return (
-            <Round
-              round={round}
-              show={roundVisibilities[index]}
-              toggleRoundVisibility={() => toggleRoundVisibility(index)}
-              key={index}
-              roundIndex={index}
-            />
-          );
-        })}
-      </motion.div>
+      <GroupResults
+        roundVisibilities={roundVisibilities}
+        areGroupResultsShown={areGroupResultsShown}
+        group={group}
+        toggleRoundVisibility={toggleRoundVisibility}
+      />
     </div>
   );
 }
