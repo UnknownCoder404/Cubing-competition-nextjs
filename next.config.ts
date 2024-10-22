4e73d13e656b5b20c32d3425e0c2b557968b85f3
Jakov <qwerasdftzu@gmail.com>
2024-10-22 21:20:18 +0200

    Make deploying to Cloudflare possible

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false /* Do not show x-powered-by header */,
 };
 
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 export default nextConfig;
