e906fe0f6dd9ea3c692fe374073a871ea230f131
Jakov <qwerasdftzu@gmail.com>
2024-10-23 21:58:50 +0200

    Add title to global errors and not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,8 @@
 import Link from "next/link";
+export const metadata = {
+  title: "Stranica nije pronađena",
+  description: "Stranica koju ste pokušali posjetiti nije pronađena.",
+};
 
 export default function NotFound() {
   return (
