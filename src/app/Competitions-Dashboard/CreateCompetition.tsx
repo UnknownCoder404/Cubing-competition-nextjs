01d23d564a6301bccf38daa1489d2efa36df5aec
Jakov <qwerasdftzu@gmail.com>
2024-10-24 22:17:25 +0200

    Cleaner code

--- src/app/Competitions-Dashboard/CreateCompetition.tsx
+++ src/app/Competitions-Dashboard/CreateCompetition.tsx
@@ -28,67 +28,41 @@ function CreateCompDialog({
         else dialogRef.current.close();
     }, [showModal]);
 
-    const handleEventChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const { name, checked } = event.target;
-        setSelectedEvents((prevSelectedEvents) => ({
-            ...prevSelectedEvents,
-            [name]: {
-                selected: checked,
-                rounds: prevSelectedEvents[name]?.rounds || 1, // Default to 1 round if not set
-            },
+    const handleEventChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = e.target;
+        setSelectedEvents((prev) => ({
+            ...prev,
+            [name]: { selected: checked, rounds: prev[name]?.rounds || 1 },
         }));
     };
 
     const handleRoundsChange = (
-        event: ChangeEvent<HTMLSelectElement>,
+        e: ChangeEvent<HTMLSelectElement>,
         eventName: string,
     ) => {
-        const { value } = event.target;
-        setSelectedEvents((prevSelectedEvents) => ({
-            ...prevSelectedEvents,
-            [eventName]: {
-                ...prevSelectedEvents[eventName],
-                rounds: parseInt(value, 10),
-            },
+        const { value } = e.target;
+        setSelectedEvents((prev) => ({
+            ...prev,
+            [eventName]: { ...prev[eventName], rounds: parseInt(value, 10) },
         }));
     };
 
-    const handleClose = () => {
-        if (dialogRef.current) {
-            dialogRef.current.close();
-        }
-        closeModal();
-    };
-
-    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
-    };
-
-    const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setDate(e.target.value);
-    };
-
     const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault(); // Prevents the form from submitting the traditional way
+        e.preventDefault();
+
+        const selectedEventList = Object.entries(selectedEvents)
+            .filter(([, { selected }]) => selected)
+            .map(([name, { rounds }]) => ({ name, rounds }));
 
         try {
-            const selectedEventList = Object.keys(selectedEvents)
-                .filter((event) => selectedEvents[event].selected)
-                .map((event) => ({
-                    name: event,
-                    rounds: selectedEvents[event].rounds,
-                }));
-
-            const compCreation = await createCompetition(
+            const { success } = await createCompetition(
                 name,
                 date,
                 selectedEventList,
             );
+            if (!success) throw new Error("Failed to create competition");
 
-            if (!compCreation.success) {
-                throw new Error("Failed to create competition");
-            }
-            handleClose(); // Close modal on successful creation
+            closeModal();
             router.refresh();
         } catch (error) {
             console.error("Error creating competition:", error);
@@ -100,41 +74,35 @@ function CreateCompDialog({
         <dialog
             className={styles["make-comp-modal"]}
             ref={dialogRef}
-            onClose={handleClose}
+            onClose={closeModal}
         >
             <form className={styles["make-comp-form"]} onSubmit={handleSubmit}>
                 <h2>Kreiraj natjecanje</h2>
                 <label htmlFor="comp-name">Ime natjecanja</label>
-                <br />
                 <input
                     type="text"
                     id="comp-name"
-                    name="name"
                     value={name}
-                    onChange={handleNameChange}
+                    onChange={(e) => setName(e.target.value)}
                     required
                 />
-                <br />
+
                 <label htmlFor="comp-date">Datum natjecanja</label>
-                <br />
                 <input
                     type="datetime-local"
                     id="comp-date"
-                    name="date"
                     value={date}
-                    onChange={handleDateChange}
+                    onChange={(e) => setDate(e.target.value)}
                     required
                 />
-                <br />
+
                 <label>Eventovi</label>
-                <br />
                 {events.map((event, index) => (
                     <div key={index}>
                         <input
                             type="checkbox"
                             id={`event-${index}`}
                             name={event}
-                            value={event}
                             onChange={handleEventChange}
                         />
                         <label htmlFor={`event-${index}`}>{event}</label>
@@ -142,24 +110,22 @@ function CreateCompDialog({
                         <label htmlFor={`rounds-${event}`}>Broj rundi</label>
                         <select
                             id={`rounds-${event}`}
-                            name={`rounds-${event}`}
                             disabled={!selectedEvents[event]?.selected}
                             value={selectedEvents[event]?.rounds || 1}
                             onChange={(e) => handleRoundsChange(e, event)}
                         >
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
+                            {[...Array(5)].map((_, i) => (
+                                <option key={i} value={i + 1}>
+                                    {i + 1}
+                                </option>
+                            ))}
                         </select>
                     </div>
                 ))}
-                <br />
                 <button type="submit" className={styles["make-comp-submit"]}>
                     Napravi
                 </button>
-                <button type="button" onClick={handleClose}>
+                <button type="button" onClick={closeModal}>
                     Zatvori
                 </button>
             </form>
@@ -169,20 +135,17 @@ function CreateCompDialog({
 
 export function CreateCompButton() {
     const [showModal, setModalVisibility] = useState(false);
-
-    function toggleModal() {
-        setModalVisibility((current) => !current);
-    }
+    const toggleModal = () => setModalVisibility((prev) => !prev);
 
     return (
         <>
             <button onClick={toggleModal}>Napravi natjecanje</button>
-            {
+            {showModal && (
                 <CreateCompDialog
                     showModal={showModal}
                     closeModal={() => setModalVisibility(false)}
                 />
-            }
+            )}
         </>
     );
 }
