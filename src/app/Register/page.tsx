120a229edd51494e826d618fd2e1f4dc50958f9e
Jakov <qwerasdftzu@gmail.com>
2024-11-05 21:31:16 +0100

    Add mock code for register page

---
+++ src/app/Register/page.tsx
@@ -0,0 +1,12 @@
+// Server component
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Registracija - Cro Cube Comp",
+    description: "Registracija korisnika za Cro Cube Comp natjecanja",
+    keywords: ["Registracija", "Cro Cube Comp"],
+};
+
+export default function Register() {
+    return <div>Register</div>;
+}
