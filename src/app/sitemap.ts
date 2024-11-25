894ec904f3de7b70b04bd4e9183472423e59e6e4
Jakov <qwerasdftzu@gmail.com>
2024-11-25 21:52:32 +0100

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,34 @@
+import type { MetadataRoute } from "next";
+
+export const dynamic = "error";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl =
+        process.env.BASE_URL || "https://cubing-competition-nextjs.vercel.app";
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/Competitions`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/Rules`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.5,
+        },
+        {
+            url: `${baseUrl}/Scramble`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.4,
+        },
+    ];
+}
